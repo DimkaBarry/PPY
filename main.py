5e3e0901da684ea0c99451fcae29368d642d8a68
s22390 <s22390@pjwstk.edu.pl>
2023-03-10 22:45:58 +0100

    Added mp3 play

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import webbrowser
-print("hi")
+import requests
+from playsound import playsound
+if __name__=='__main__':
+    playsound('C418.mp3')
+
+    #response = requests.get("http://pja.edu.pl/")
+
